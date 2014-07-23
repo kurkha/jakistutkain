9b172c74130c068f6cde439ce5f887d15e2c6785
kurkha <kurkha@gmail.com>
2014-07-23 15:43:58 +0300

    Start of repo

---
+++ PhoneApp1/MatchData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp1
+{
+    class MatchData
+    {
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+
+        public string HomeTeamLogoAddress { get; set; }
+
+        public string AwayTeamLogoAddress { get; set; }
+
+        public string HomeGoals { get; set; }
+
+        public string AwayGoals { get; set; }
+
+        public string MatchDate { get; set; }
+
+
+
+    }
+
+}
---
+++ PhoneApp1/MatchFetcher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using AdvancedREI.Net.Http.Compression;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace PhoneApp1
+{
+    class IOandConversion
+    {
+
+
+        public Task<string> readCompressedHtmlPage(String pageAdress)
+        {
+            CompressedHttpClientHandler handler = new CompressedHttpClientHandler();
+            HttpClient client = new HttpClient(handler);
+
+            //TODO Actually should get whole GET response with GetAsync and check that status code is valid.
+            Task<string> response = client.GetStringAsync(pageAdress);
+
+            return response;
+        }
+
+        public  readJsonTextToDataset(String text)
+        {
+            
+
+
+        }
+
+    }
+}
